9b7ed79f434bd067a6c36f9e50b978c98f84da1b
bonabrian <bonabriansiagian@gmail.com>
2022-06-26 01:46:31 +0700

    feat: implement global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,29 @@
-import '../styles/globals.css'
-
+import type { EmotionCache } from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { createEmotionCache } from '@/common/utils'
+import { GlobalStyles } from '@/styles'
+
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const MyApp = ({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      {/* TODO: implement ThemeProvider from `emotion/react`
+      <ThemeProvider theme={theme}>...rest</ThemeProvider> */}
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
 }
 
 export default MyApp
